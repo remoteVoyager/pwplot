024e7317a5a62cc7a04c237bf038c6af2dad30a6
remoteVoyager <mlukaszewicz2@gmail.com>
2020-01-06 15:13:41 +0100

    comments updated

--- pwplot.py
+++ pwplot.py
@@ -5,28 +5,33 @@ import os
 
 def multi_plot(data, argum, file_format='png', var_s=None, var_s_uni=None, var_jedn=None, show_grid = False, show_ptlabels=False, approx=False, approx_ord=2):
     """ Function plotting multiple plots for given data """
-    # dla nie zadanego var_s pobierz nazwy kolumn z danych
     
+    # if var_s not provided use dataframe column values
     if var_s is None:
         var_s = data[0].columns.values
 
+    # if alternate names not provided use dataframe column names
     if var_s_uni is None:
         var_s_uni = var_s
 
+    # if units not porvided use empty string 
     if var_jedn is None:
-        var_jedn = [' ' for i in range(len(var_s))]
+        var_jedn = ['' for i in range(len(var_s))]
 
     if not os.path.exists('plots/'):
         os.mkdir('plots')
 
-    # wykres
+    # plotting
     for s, suni, jedn in zip(var_s, var_s_uni, var_jedn):
+        
+        # figure size definition (currently A4 portrait)
+        # TODO: multiple size plots
         plt.figure(figsize=[8.3, 11.7])
 
         print("Ploting {} = f({}) to /plots".format(s, argum))
 
+        # plot for each dataset
         for dat in data:
-
             if approx:
                 plt.plot(dat[argum], dat[s], 'b.')
 
@@ -58,10 +63,13 @@ def multi_plot(data, argum, file_format='png', var_s=None, var_s_uni=None, var_j
             
             plt.grid(show_grid)
 
+
+        # main formatting
         plt.xlabel("I\N{SUBSCRIPT TWO} [A]")
         plt.ylabel("{}  [{}]".format(suni, jedn))
         plt.title("Wykres {} w zależności od I\N{SUBSCRIPT TWO}".format(suni))
             
+        # plot saving
         plt.savefig("plots/plot_{}.{}".format(s, file_format), bbox_inches='tight', pad_inches=0.3)
 
         print("Succesfully plotted {} = f({})".format(s, 'P2'))
