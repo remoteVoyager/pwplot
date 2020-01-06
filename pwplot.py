3419a658b1c08141a22b500fb0099b6f0ff39a67
remoteVoyager <mlukaszewicz2@gmail.com>
2020-01-06 15:08:00 +0100

    Code changed to function form, base working
    added .gitignore

--- pwplot.py
+++ pwplot.py
@@ -3,83 +3,68 @@ import pandas as pd
 import numpy as np  
 import os
 
+def multi_plot(data, argum, file_format='png', var_s=None, var_s_uni=None, var_jedn=None, show_grid = False, show_ptlabels=False, approx=False, approx_ord=2):
+    """ Function plotting multiple plots for given data """
+    # dla nie zadanego var_s pobierz nazwy kolumn z danych
+    
+    if var_s is None:
+        var_s = data[0].columns.values
 
-data = [pd.read_csv('dane.csv')]
-argum = 'I2'    # argument funkcji
+    if var_s_uni is None:
+        var_s_uni = var_s
 
-file_format = 'pdf'
+    if var_jedn is None:
+        var_jedn = [' ' for i in range(len(var_s))]
 
-# plot formatting
+    if not os.path.exists('plots/'):
+        os.mkdir('plots')
 
-approx = False  # czy aproksymować?
-approx_ord = 2  # stopień wielomianu do aproksymacji
+    # wykres
+    for s, suni, jedn in zip(var_s, var_s_uni, var_jedn):
+        plt.figure(figsize=[8.3, 11.7])
 
-set_grid = True
+        print("Ploting {} = f({}) to /plots".format(s, argum))
 
-show_ptlabels = True
+        for dat in data:
 
+            if approx:
+                plt.plot(dat[argum], dat[s], 'b.')
 
-if not os.path.exists('plots/'):
-    os.mkdir('plots')
+                x = dat[argum].to_numpy().astype(np.float)
+                y = dat[s].to_numpy().astype(np.float)
 
-var_s = [ 'P1', 'P2', 'I1', 'U2', 'cosphi', 'ni']
-var_s_uni = ['P\N{SUBSCRIPT ONE}', 'P\N{SUBSCRIPT ONE}', 'I\N{SUBSCRIPT ONE}', 'U\N{SUBSCRIPT TWO}', 'cos\N{GREEK SMALL LETTER PHI}', '\u03b7']
-var_jedn = ['W', 'W', 'A', 'V', '\N{DEGREE SIGN}', '%']
-ascds = []
+                f = np.polyfit(x, y, approx_ord)
+                fp = np.poly1d(f)
 
-# dla nie zadanego var_s pobierz nazwy kolumn z danych
-if var_s is None:
-    var_s = data[0].columns.values
+                xnew = np.linspace(x.min(), x.max(), 100)
 
-if ascds is None or ascds == []:
-    ascds = [None for i in range(len(var_s))]
+                plt.plot(xnew, fp(xnew), 'r')
 
-# wykres
-for s, suni, jedn, ascd in zip(var_s, var_s_uni, var_jedn, ascds):
-    plt.figure(figsize=[8.3, 11.7])
+                plt.legend(suni, suni + 'aproksymacja w. st. {}'.format(approx_ord))
 
-    print("Ploting {} = f({}) to /plots".format(s, argum))
+            elif show_ptlabels:
+                plt.plot(dat[argum], dat[s], 'bo-')
+                
+                for x, y in zip(dat[argum], dat[s]):
 
-    for dat in data:
+                    plt.annotate(y, # this is the text
+                            (x,y), # this is the point to label
+                            textcoords="offset points", # how to position the text
+                            xytext=(0,10), # distance from text to points (x,y)
+                            ha='center') # horizontal alignment can be left, right or center
 
-        if approx:
-            plt.plot(dat[argum], dat[s], 'b.')
-
-            x = dat[argum].to_numpy().astype(np.float)
-            y = dat[s].to_numpy().astype(np.float)
-
-            f = np.polyfit(x, y, 2)
-            fp = np.poly1d(f)
-
-            xnew = np.linspace(x.min(), x.max(), 100)
-
-            plt.plot(xnew, fp(xnew), 'r')
-
-            plt.legend(suni, suni + 'aproksymacja w. st. {}'.format(approx_ord))
-
-        elif show_ptlabels:
-            plt.plot(dat[argum], dat[s], 'bo-')
+            else:
+                plt.plot(dat[argum], dat[s], 'b')
             
-            for x, y in zip(dat[argum], dat[s]):
+            plt.grid(show_grid)
 
-                plt.annotate(y, # this is the text
-                        (x,y), # this is the point to label
-                        textcoords="offset points", # how to position the text
-                        xytext=(0,10), # distance from text to points (x,y)
-                        ha='center') # horizontal alignment can be left, right or center
+        plt.xlabel("I\N{SUBSCRIPT TWO} [A]")
+        plt.ylabel("{}  [{}]".format(suni, jedn))
+        plt.title("Wykres {} w zależności od I\N{SUBSCRIPT TWO}".format(suni))
+            
+        plt.savefig("plots/plot_{}.{}".format(s, file_format), bbox_inches='tight', pad_inches=0.3)
 
-        else:
-            plt.plot(dat[argum], dat[s], 'b')
-        
-        plt.grid(set_grid)
+        print("Succesfully plotted {} = f({})".format(s, 'P2'))
 
-    plt.xlabel("I\N{SUBSCRIPT TWO} [A]")
-    plt.ylabel("{}  [{}]".format(suni, jedn))
-    plt.title("Wykres {} w zależności od I\N{SUBSCRIPT TWO}".format(suni))
-        
-    plt.savefig("plots/plot_{}.{}".format(s, file_format), bbox_inches='tight', pad_inches=0.3)
-
-    print("Succesfully plotted {} = f({})".format(s, 'P2'))
-
-print("Plotting Finished")
+    print("Plotting Finished")
 
